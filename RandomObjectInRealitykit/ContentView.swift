a48e03a01e3290a29cad00b0cc8bb442ec176935
sinano1107 <tftf161107@icloud.com>
2022-12-02 17:28:44 +0900

    nonARでinstallGestureによるキューブ回転

--- RandomObjectInRealitykit/ContentView.swift
+++ RandomObjectInRealitykit/ContentView.swift
@@ -16,15 +16,19 @@ struct ContentView: View {
 
 struct ARViewContainer: UIViewRepresentable {
     func makeUIView(context: Context) -> ARView {
+        #if targetEnvironment(simulator)
         let arView = ARView(frame: .zero)
+        #else
+        let arView = ARView(frame: .zero, cameraMode: .nonAR, automaticallyConfigureSession: false)
+        #endif
         
-        let newAnchor = AnchorEntity(plane: .horizontal)
-        let newBox = ModelEntity(mesh: .generateBox(size: 0.3))
+        let newAnchor = AnchorEntity(world: [0, 0, -1])
+        let newBox = ModelEntity(mesh: .generateBox(size: 1))
         newBox.generateCollisionShapes(recursive: true)
         newAnchor.addChild(newBox)
         arView.scene.addAnchor(newAnchor)
         
-        arView.installGestures(for: newBox)
+        arView.installGestures(.rotation, for: newBox)
         
         return arView
     }
