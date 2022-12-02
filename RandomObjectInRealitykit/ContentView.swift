b33a9cc992347306f35b8c2ee90ae221b1397ad0
sinano1107 <tftf161107@icloud.com>
2022-12-02 15:41:12 +0900

    Initial Commit

---
+++ RandomObjectInRealitykit/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  RandomObjectInRealitykit
+//
+//  Created by 長政輝 on 2022/12/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RandomObjectInRealitykit/RandomObjectInRealitykitApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomObjectInRealitykitApp.swift
+//  RandomObjectInRealitykit
+//
+//  Created by 長政輝 on 2022/12/02.
+//
+
+import SwiftUI
+
+@main
+struct RandomObjectInRealitykitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
