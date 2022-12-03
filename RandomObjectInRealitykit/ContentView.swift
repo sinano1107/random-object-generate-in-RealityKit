37fc45a45a21f47c731a2a44704d596aec374201
sinano1107 <tftf161107@icloud.com>
2022-12-03 18:43:05 +0900

    OrbitControlを自作

--- RandomObjectInRealitykit/ContentView.swift
+++ RandomObjectInRealitykit/ContentView.swift
@@ -8,20 +8,55 @@
 import SwiftUI
 import RealityKit
 
-let newBox = ModelEntity(mesh: .generateBox(size: 1))
+let cameraAnchor = AnchorEntity(world: .zero)
+let camera = PerspectiveCamera()
+let newAnchor = AnchorEntity(world: .zero)
+let newBox = ModelEntity(mesh: .generateBox(size: 0.2))
+
+var mouse: simd_float2 = [0, 0]
+var nowPos = camera.position
+var pos: simd_float3 = [0, 0, 0]
+var prevX: CGFloat?
+var prevY: CGFloat?
+var prevTime: Date?
+func orbit(value: DragGesture.Value) {
+    let deltaTime = prevTime != nil ? value.time.timeIntervalSince(prevTime!) : 0
+    prevTime = value.time
+    
+    let deltaX = prevX != nil ? Float(value.location.x - prevX!) : 0
+    let deltaY = prevY != nil ? Float(value.location.y - prevY!) : 0
+    prevX = value.location.x
+    prevY = value.location.y
+
+    mouse += vector2(deltaX, deltaY) * Float(deltaTime) * 0.3
+    print(mouse)
+    
+    mouse.y = min(max(mouse.y, 0), 1)
+    
+    pos.x = -1 * sin(mouse.y * .pi) * cos(mouse.x * .pi)
+    pos.y = -1 * cos(mouse.y * .pi)
+    pos.z = -1 * sin(mouse.y * .pi) * sin(mouse.x * .pi)
+    
+    camera.look(at: newBox.position(relativeTo: nil), from: pos, relativeTo: nil)
+}
 
 struct ContentView: View {
     @State var value: Float = 1
     
     var body: some View {
         VStack {
-            ARViewContainer().edgesIgnoringSafeArea(.all)
+            ARViewContainer()
+                .edgesIgnoringSafeArea(.all)
+                .gesture(DragGesture().onChanged(orbit).onEnded({ value in
+                    prevX = nil
+                    prevY = nil
+                    prevTime = nil
+                }))
             Slider(value: Binding(get: { value }, set: { newValue in
                 newBox.setScale(SIMD3(repeating: newValue), relativeTo: nil)
                 value = newValue
             }), in: 0.1...2).padding(.horizontal)
         }
-        
     }
 }
 
@@ -32,9 +67,10 @@ struct ARViewContainer: UIViewRepresentable {
         #else
         let arView = ARView(frame: .zero, cameraMode: .nonAR, automaticallyConfigureSession: false)
         #endif
-        
-        let newAnchor = AnchorEntity(world: [0, 0, -1])
-        newBox.generateCollisionShapes(recursive: true)
+
+        cameraAnchor.addChild(camera)
+        arView.scene.addAnchor(cameraAnchor)
+
         newAnchor.addChild(newBox)
         arView.scene.addAnchor(newAnchor)
         
